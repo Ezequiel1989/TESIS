561302482d5b6ec5725c8673ca2a6ee2647b02d0
echemdq <eche.mdq@hotmail.com>
2015-01-13 18:43:48 -0300

    hibernate
    agregue la cfg de hibernate, y cree una clase de mapeo y un modelo estoy
    probando...

---
+++ TESIS - SISTEMA DE CONTROL/src/DAO/UsuarioDAO.java
@@ -0,0 +1,70 @@
+package DAO;
+
+import Modelo.Usuario;
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import tesis.sistema.de.control.HibernateUtil;
+
+/**
+ *
+ * @author H Nisenbaum
+ */
+
+
+/**
+ * Esta clase representa el acceso a datos de Usuario
+ * @author Administrador
+ */
+public class UsuarioDAO {
+
+    private Session sesion;
+    private Transaction tx;
+
+    /**
+     * Este metodo se encarga de abrir la session y comenzar una nueva
+     * transaccion
+     *
+     * @throws HibernateException
+     */
+    private void iniciaOperacion() throws HibernateException {
+        sesion = HibernateUtil.getSessionFactory().openSession();
+        tx = sesion.beginTransaction();
+    }
+
+    /**
+     * Este metodo hace rollback a la transaccion en caso de excepcion.
+     *
+     * @param La excepcion capturada
+     * @throws HibernateException
+     */
+    private void manejaExcepcion(HibernateException he) throws HibernateException {
+        tx.rollback();
+        throw new HibernateException("Ocurrió un error en la capa de acceso a datos", he);
+    }
+
+    /**
+     * Este metodo se encarga de confirmar que el usuario sea valido.
+     *
+     * @param user
+     * @param pass
+     * @return Devuelve un usuario
+     * @throws HibernateException
+     */
+    public Usuario confirmarUsuario(String user, String pass) throws HibernateException {
+        Usuario usuario = null;
+        String hql = "from Modelo.Usuario where USUARIO='" + user + "' AND PASSWORD='" + pass + "'";
+        try {
+            iniciaOperacion();
+            Query query = sesion.createQuery(hql);
+            usuario = (Usuario) query.uniqueResult();
+        } catch (HibernateException he) {
+            manejaExcepcion(he);
+            throw he;
+        } finally {
+            sesion.close();
+        }
+        return usuario;
+    }
+}
---
+++ TESIS - SISTEMA DE CONTROL/src/Modelo/Usuario.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+/**
+ *
+ * @author Ezequiel
+ */
+public class Usuario {
+    private long id_usuario;
+    private String usuario;
+    private String password;
+
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public long getId_usuario() {
+        return id_usuario;
+    }
+
+    public void setId_usuario(long id_usuario) {
+        this.id_usuario = id_usuario;
+    }
+}
