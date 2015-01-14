6da8c5e94530903d690daa4e6d8c175ac40b9211
Ezequiel1989 <eze.cristeche@gmail.com>
2015-01-14 19:11:43 -0300

    Class Localidad

--- TESIS - SISTEMA DE CONTROL/src/Modelo/Localidad.java
+++ TESIS - SISTEMA DE CONTROL/src/Modelo/Localidad.java
@@ -10,5 +10,50 @@ package Modelo;
  * @author matias
  */
 public class Localidad {
-    
+
+    private long idlocalidad;
+    private String localidad;
+    private Empleado oempleado;
+
+    /**
+     * @return the idlocalidad
+     */
+    public long getIdlocalidad() {
+        return idlocalidad;
+    }
+
+    /**
+     * @param idlocalidad the idlocalidad to set
+     */
+    public void setIdlocalidad(long idlocalidad) {
+        this.idlocalidad = idlocalidad;
+    }
+
+    /**
+     * @return the localidad
+     */
+    public String getLocalidad() {
+        return localidad;
+    }
+
+    /**
+     * @param localidad the localidad to set
+     */
+    public void setLocalidad(String localidad) {
+        this.localidad = localidad;
+    }
+
+    /**
+     * @return the oempleado
+     */
+    public Empleado getOempleado() {
+        return oempleado;
+    }
+
+    /**
+     * @param oempleado the oempleado to set
+     */
+    public void setOempleado(Empleado oempleado) {
+        this.oempleado = oempleado;
+    }
 }
