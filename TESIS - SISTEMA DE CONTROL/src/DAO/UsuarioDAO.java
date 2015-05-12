f6237912ebc89a770767f17b0b46327771f681b7
echemdq <eche.mdq@hotmail.com>
2015-05-12 12:20:16 -0300

    anda jasper LTA

--- TESIS - SISTEMA DE CONTROL/src/DAO/UsuarioDAO.java
+++ TESIS - SISTEMA DE CONTROL/src/DAO/UsuarioDAO.java
@@ -1,22 +1,36 @@
 package DAO;
 
 import Modelo.Usuario;
+import java.text.SimpleDateFormat;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Date;
 import org.hibernate.HibernateException;
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import tesis.sistema.de.control.HibernateUtil;
-
+import net.sf.jasperreports.engine.query.JRHibernateQueryExecuterFactory;
+import net.sf.jasperreports.view.JasperViewer;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JRException;
 /**
  *
  * @author Ezequiel
  */
 /**
-*/
+ */
 public class UsuarioDAO {
 
     private Session sesion;
     private Transaction tx;
+    private final Map<String, Object> param = new HashMap<>();
+    private SimpleDateFormat sf = new SimpleDateFormat("dd-M-yyyy@HH.mm.ss");
+    private Date date = new Date();
 
     /**
      * Este metodo se encarga de abrir la session y comenzar una nueva
@@ -24,11 +38,25 @@ public class UsuarioDAO {
      *
      * @throws HibernateException
      */
-    private void iniciaOperacion() throws HibernateException
-    
-    {
+    private void iniciaOperacion() throws HibernateException {
         sesion = HibernateUtil.getSessionFactory().openSession();
         tx = sesion.beginTransaction();
+    }
+
+    public void report(String path, String fileName) {
+        try {
+            iniciaOperacion();
+            param.put(JRHibernateQueryExecuterFactory.PARAMETER_HIBERNATE_SESSION, sesion);
+            String file = "pdf\\" + fileName + sf.format(date.getTime()) + ".pdf";            
+            JasperReport jRpt = JasperCompileManager.compileReport(path);
+            JasperPrint jPrint = JasperFillManager.fillReport(jRpt, param);
+            JasperViewer.viewReport(jPrint, false);
+            JasperExportManager.exportReportToPdfFile(jPrint, file);
+        } catch (JRException ex) {
+            System.out.println(ex.getMessage());
+        } finally {
+            sesion.close();
+        }
     }
 
     /**
@@ -37,7 +65,7 @@ public class UsuarioDAO {
      * @param La excepcion capturada
      * @throws HibernateException
      */
-    
+
     private void manejaExcepcion(HibernateException he) throws HibernateException {
         tx.rollback();
         throw new HibernateException("Ocurrió un error en la capa de acceso a datos", he);
@@ -51,9 +79,7 @@ public class UsuarioDAO {
      * @return Devuelve un usuario
      * @throws HibernateException
      */
-    public Usuario confirmarUsuario(String user, String pass) throws HibernateException 
-    
-    {
+    public Usuario confirmarUsuario(String user, String pass) throws HibernateException {
         Usuario usuario = null;
         String hql = "from Modelo.Usuario where USUARIO='" + user + "' AND PASSWORD='" + pass + "'";
         try {
@@ -68,6 +94,5 @@ public class UsuarioDAO {
         }
         return usuario;
     }
-    
-    
+
 }
--- TESIS - SISTEMA DE CONTROL/src/tesis/sistema/de/control/Logueo.java
+++ TESIS - SISTEMA DE CONTROL/src/tesis/sistema/de/control/Logueo.java
@@ -38,6 +38,7 @@ public class Logueo extends javax.swing.JFrame {
         btnIngresar = new javax.swing.JButton();
         txt_usuario = new javax.swing.JTextField();
         txt_password = new javax.swing.JPasswordField();
+        jButton1 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -63,24 +64,36 @@ public class Logueo extends javax.swing.JFrame {
             }
         });
 
+        jButton1.setText("jButton1");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(lblcontraseña, javax.swing.GroupLayout.DEFAULT_SIZE, 94, Short.MAX_VALUE)
-                    .addComponent(lblusuario, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                    .addComponent(txt_password, javax.swing.GroupLayout.DEFAULT_SIZE, 90, Short.MAX_VALUE)
-                    .addComponent(txt_usuario))
-                .addGap(120, 120, 120))
-            .addGroup(layout.createSequentialGroup()
                 .addGap(146, 146, 146)
                 .addComponent(btnIngresar)
                 .addContainerGap(181, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(lblcontraseña, javax.swing.GroupLayout.DEFAULT_SIZE, 94, Short.MAX_VALUE)
+                            .addComponent(lblusuario, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addGap(18, 18, 18)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                            .addComponent(txt_password, javax.swing.GroupLayout.DEFAULT_SIZE, 90, Short.MAX_VALUE)
+                            .addComponent(txt_usuario))
+                        .addGap(120, 120, 120))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(jButton1)
+                        .addGap(43, 43, 43))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -95,7 +108,9 @@ public class Logueo extends javax.swing.JFrame {
                     .addComponent(txt_password, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(30, 30, 30)
                 .addComponent(btnIngresar)
-                .addContainerGap(72, Short.MAX_VALUE))
+                .addGap(18, 18, 18)
+                .addComponent(jButton1)
+                .addContainerGap(31, Short.MAX_VALUE))
         );
 
         pack();
@@ -130,6 +145,11 @@ public class Logueo extends javax.swing.JFrame {
                 txt_usuario.requestFocus();
             }
     }//GEN-LAST:event_txt_passwordKeyReleased
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+        DAOUsuario.report("listados\\usuarios1.jrxml", "Report-");
+    }//GEN-LAST:event_jButton1ActionPerformed
     public void entrar()
     {
         try {            
@@ -155,6 +175,7 @@ public class Logueo extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnIngresar;
+    private javax.swing.JButton jButton1;
     private javax.swing.JLabel lblcontraseña;
     private javax.swing.JLabel lblusuario;
     private javax.swing.JPasswordField txt_password;
